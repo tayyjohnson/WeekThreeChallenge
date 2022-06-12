2ca496f67bf77bbbbf48967e64135e68b91231f9
tayyjohnson <taydriver1@gmail.com>
2022-06-12 18:00:16 -0400

    added some js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
 // Assignment code here
 
+//variables used for random password
+var numberChars="0123456789";
+var upperChars= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerChars= "abcdefghijklmnopqrstuvwxyz";
+var specialChars= "!@#$%^&*?<>-_"
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
