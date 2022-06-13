6fada582bd5cf7ae2919f17ea58ef567cbccdc2f
tayyjohnson <taydriver1@gmail.com>
2022-06-13 18:33:27 -0400

    issue with the button

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,41 @@ var upperChars= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerChars= "abcdefghijklmnopqrstuvwxyz";
 var specialChars= "!@#$%^&*?<>-_"
 
+//password length requirements
+function userOptions() {
+  var length=prompt ("How long is your password required to be?");
+    if (length <8 ){
+      alert("Please choose a password that is greater than 8 characters.")
+      return null;
+    }
+    if (length >50) {
+      alert("Please choose a password that is 50 characters or less.")
+      return null;
+    }
 
+//determining other special password requirements
+
+var confirmLowerCase= confirm("Does your password require lowercase letters?");
+var confirmUpperCase= confirm("Does your password require uppercase letters?");
+var confirmNumber= confirm("Does your password require numbers?");
+var confirmSpecial= confirm("Does your password require any special characters?");
+
+var allChars = {
+  length:length,
+  confirmLowerCase:confirmLowerCase,
+  confirmUpperCase:confirmUpperCase,
+  confirmNumber:confirmNumber,
+  confirmSpecial:confirmSpecial,
+}
+return allChars;
+
+};
+
+function randomChars(Array) {
+  var randomIndex = Math.floor(Math.random() * Array.length);
+  var randomElement = Array[randomIndex];
+  return randomElement;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -18,6 +52,10 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  var userChoice = userOptions();
 }
 
 // Add event listener to generate button
